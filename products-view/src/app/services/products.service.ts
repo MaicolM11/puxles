b8e0809ca47db4e2b1957257f7507dadbaae0d91
MaicolM11 <mmoarias4@gmail.com>
2022-07-12 15:41:42 -0500

    product service

--- products-view/src/app/services/products.service.ts
+++ products-view/src/app/services/products.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { Product } from '../models/Product';
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class ProductsService {
 
-  constructor() { }
+  url: string = 'https://localhost:8080/products';
+
+  constructor(private http: HttpClient) { }
+
+  getProducts(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.url);;
+  }
+
+  createProduct(product : Product): Observable<Product> {
+    return this.http.post<Product>(this.url, product);
+  }
+
+  updateProduct(product : Product, id:number): Observable<Product> {
+    return this.http.put<Product>(`${this.url}/${id}` , product);
+  }
+
+  deleteProduct(id: number): Observable<any> {
+    return this.http.delete(`${this.url}/${id}`)
+  }
+
 }
